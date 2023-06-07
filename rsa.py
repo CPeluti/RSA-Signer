a776e1e026f9a57affe564272e53ae751358f702
Caio Peluti <cpeluti@gmail.com>
2023-06-07 11:59:01 -0300

    rsa started

--- rsa.py
+++ rsa.py
@@ -1,2 +1,35 @@
+import random
+
+def millerRabin(d,n):
+    a  = 2 + random.randint(1,(n-4))
+    x = pow(a,d,n)
+    if(x==n-1 or x ==1): return True
+    while(d!=n-1):
+        x = (x*x) % n
+        d *= 2
+        if(x==1): return False
+        if (x==n-1): return True
+    return False
+def isPrime(n, k):
+    if (n <= 1 or n == 4):
+        return False;
+    if n <= 3:
+        return True
+    d = n-1
+    while(d%2 == 0):
+        d //= 2
+
+    for i in range(k):
+        if(not millerRabin(d,n)): return False
+    return True
+
+def genPrime():
+    while(True):
+        num = random.randint(2**1024, 2**2048)
+        print(f"trying {num}")
+        if(isPrime(num,15)):
+            print(num)
+            return num
 def run():
+    genPrime()
     print("rsa")
---
+++ test_rsa.py
@@ -0,0 +1,3 @@
+import rsa
+def test_isPrime():
+    assert rsa.isPrime(5,40) == True
