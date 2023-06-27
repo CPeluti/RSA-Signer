74f656b6bb594bea064cff1796ffb3a369b11a97
AnaJordao <acjpjvjp@gmail.com>
2023-06-27 16:19:19 -0300

    Geraçao das chaves publicas feitas

--- rsa.py
+++ rsa.py
@@ -10,6 +10,7 @@ def millerRabin(d,n):
         if(x==1): return False
         if (x==n-1): return True
     return False
+
 def isPrime(n, k):
     if (n <= 1 or n == 4):
         return False;
@@ -26,10 +27,61 @@ def isPrime(n, k):
 def genPrime():
     while(True):
         num = random.randint(2**1024, 2**2048)
-        print(f"trying {num}")
+        # print(f"trying {num}")
         if(isPrime(num,15)):
-            print(num)
             return num
+        
+def extendEuclid(a, b):
+    # a * x + b * y = euclid(a, b)
+    print(a, b)
+    if a == 0:
+        return b, 0, 1
+    
+    gcd,x1,y1 = extendEuclid(b%a, a)
+
+    x = y1 - (b//a) * x1
+    y = x1
+     
+    return gcd,x,y
+    
+def invertedModule(a, b):
+    # d * e = 1 (mod lcm) <=> d * e + k * lcm = 1 
+    gcd, x, y = extendEuclid(a, b)
+
+    if (gcd != 1):
+        return None
+    else:
+        return (x % b + b) % b
+    
+def genPrivateKey(e, p, q):
+    phi = (p-1)*(q-1)
+    
+    d = invertedModule(e, phi)
+    return d
+    
+
+def genKey(p, q, e=None):
+    #public key
+    if(e == None):
+        while(isPrime(e, phi)):
+            e = random.randrange(1, phi)
+    n = p*q
+
+    # private key
+    d = genPrivateKey(e, p, q)
+    return ((e, n), (d, n)) # (public, private)
+
 def run():
-    genPrime()
-    print("rsa")
+    # gera os dois números primos
+    # p = genPrime()
+    # q = genPrime()
+    # e = 65537
+    # print(f"NUMERO P = {p}")
+    # print(f"NUMERO Q = {q}")
+    p = 5
+    q = 17
+    e = 11
+
+    k = genKey(p, q, e)
+    print(k)
+    return
